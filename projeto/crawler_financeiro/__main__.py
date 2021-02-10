7704c65f6693ca5f45be986455ae95556be3bcfa
João Paulo Carvalho <jjpaulo2@protonmail.com>
2021-02-10 00:43:25 -0300

    corrigindo nome do arquivo de cores, criando arquivo executável do bash e adicionando doctring ao __main__.py

--- projeto/crawler_financeiro/__main__.py
+++ projeto/crawler_financeiro/__main__.py
@@ -1,7 +1,16 @@
+"""
+Arquivo que será executado quando o módulo for chamado por linha de comando.
+    python -m crawler_financeiro
+
+O procedimento executado é:
+    1. Faz o download dos JSONs com os dados iniciais. (`json_downloader.py`)
+    2. Filtra os dados apenas para os valos interessados. (`json_downloader.py`)
+    3. Utiliza os dados filtrados para fazer download dos PDFs com os regulamentos. (`pdf_downloader.py`)
+"""
 from . import json_downloader
 from . import pdf_downloader
 
-from .colors import ANSIColors
+from .ansi_colors import ANSIColors
 
 print(f"""
 ==================================
